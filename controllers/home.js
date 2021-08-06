2f27742fa4e0da3081503ebd3d03639f3cf516c0
berneybcc <57729351+berneybcc@users.noreply.github.com>
2021-08-06 09:18:29 -0500

    Se crea controlador Home

---
+++ controllers/home.js
@@ -0,0 +1,8 @@
+function homesite(req,res){
+    return res.status(200).send('Home Nodejs Firebase');
+}
+
+module.exports={
+    homesite
+};
+  
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var HomeController = require('../controllers/home');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/home',HomeController.homesite)
 
 module.exports = router;
